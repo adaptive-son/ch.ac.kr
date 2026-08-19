a42cf8422b6dc0b79503e32e36cff11a5da886cd
jhko <jhko@adaptive.co.kr>
2026-08-19 11:39:21 +0900

    global2 CMS 콘텐츠 타이포그래피 및 intro 페이지 링크 보강
    - title-type0x/word-type01/ul-list0x/ol-list01 등 CMS 콘텐츠 클래스에
      스타일이 아예 없어 불릿/제목이 안 보이던 문제 조치, 참조 사이트에서
      타이포그래피 모듈과 불릿 아이콘 이식
      (파일명에 '@'가 있으면 dev 서버 WAF가 400을 반환해 언더스코어로 리네임)
    - intro.php/intro2.php의 글로벌센터, 국제개발협력센터 카드가 숨김 처리된
      상위 메뉴 밑에 있어 href="#"로 남아있던 문제, $find_2depth로 실제 링크 연결

--- pub/global2/main/intro.php
+++ pub/global2/main/intro.php
@@ -81,19 +81,21 @@ $guide_menu_rs = $adb->query("SELECT * FROM guide_menu WHERE site_id='global2' A
 				<!-- 글로벌센터 -->
 				<div class="intro-card card--sky">
 					<div class="card-head">
+						<a href="<?=$find_2depth[16639][LINK_URL]?>">
 						<strong class="card-title">글로벌센터</strong>
 						<span class="card-title-en">Global Center</span>
+						</a>
 					</div>
 					<div class="card-body">
 						<ul class="card-menu">
-							<li><a href="#">글로벌센터 소개</a></li>
-							<li><a href="#">영어 교육</a></li>
-							<li><a href="#">해외 연수</a></li>
-							<li><a href="#">글로벌 현장학습</a></li>
-							<li><a href="#">공지사항</a></li>
-							<li><a href="#">FAQ</a></li>
-							<li><a href="#">포토갤러리</a></li>
-							<li><a href="#">자료실</a></li>
+							<li><a href="<?=$find_2depth[16639][LINK_URL]?>">글로벌센터 소개</a></li>
+							<li><a href="<?=$find_2depth[16640][LINK_URL]?>">영어 교육</a></li>
+							<li><a href="<?=$find_2depth[16641][LINK_URL]?>">해외 연수</a></li>
+							<li><a href="<?=$find_2depth[16642][LINK_URL]?>">글로벌 현장학습</a></li>
+							<li><a href="<?=$find_2depth[16643][LINK_URL]?>">공지사항</a></li>
+							<li><a href="<?=$find_2depth[16644][LINK_URL]?>">FAQ</a></li>
+							<li><a href="<?=$find_2depth[16645][LINK_URL]?>">포토갤러리</a></li>
+							<li><a href="<?=$find_2depth[16646][LINK_URL]?>">자료실</a></li>
 						</ul>
 						<div class="card-icon" aria-hidden="true">
 							<img src="../img/intro/icon_intro03.png" alt="" />
@@ -104,18 +106,20 @@ $guide_menu_rs = $adb->query("SELECT * FROM guide_menu WHERE site_id='global2' A
 				<!-- 국제개발협력센터 -->
 				<div class="intro-card card--purple">
 					<div class="card-head">
+						<a href="<?=$find_2depth[16648][LINK_URL]?>">
 						<strong class="card-title">국제개발협력센터</strong>
 						<span class="card-title-en">International Development Cooperation Center</span>
+						</a>
 					</div>
 					<div class="card-body">
 						<ul class="card-menu">
-							<li><a href="#">국제개발협력센터 소개</a></li>
-							<li><a href="#">코이카 중점 사업</a></li>
-							<li><a href="#">개발협력사업</a></li>
-							<li><a href="#">공지사항</a></li>
-							<li><a href="#">FAQ</a></li>
-							<li><a href="#">포토갤러리</a></li>
-							<li><a href="#">자료실</a></li>
+							<li><a href="<?=$find_2depth[16648][LINK_URL]?>">국제개발협력센터 소개</a></li>
+							<li><a href="<?=$find_2depth[16649][LINK_URL]?>">코이카 중점 사업</a></li>
+							<li><a href="<?=$find_2depth[16650][LINK_URL]?>">개발협력사업</a></li>
+							<li><a href="<?=$find_2depth[16651][LINK_URL]?>">공지사항</a></li>
+							<li><a href="<?=$find_2depth[16652][LINK_URL]?>">FAQ</a></li>
+							<li><a href="<?=$find_2depth[16653][LINK_URL]?>">포토갤러리</a></li>
+							<li><a href="<?=$find_2depth[16654][LINK_URL]?>">자료실</a></li>
 						</ul>
 						<div class="card-icon" aria-hidden="true">
 							<img src="../img/intro/icon_intro04.png" alt="" />
